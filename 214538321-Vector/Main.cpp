c2e38f449fc5b90f1f4e775d9a4cc61f157ea1b5
samkelo <sumcarelow@gmail.com>
2016-04-16 19:09:43 +0200

    final commit

--- 214538321-Vector/Main.cpp
+++ 214538321-Vector/Main.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
 int main(){
+	vector<int> Sam;
 
+	for (int i = 0; i <= 19; i++)
+	{
+		Sam.push_back(rand() % 100 + 1);
+	}
 
-
+	sort(Sam.begin(), Sam.end());
 
 	return 0;
 }
